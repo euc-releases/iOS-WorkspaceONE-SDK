95fb622625cc68fa027571896376cabacb56ebe7
Vmware SDK Bot Swift Package Publisher <vmwaresdkbot@gmail.com>
2021-09-15 01:24:48 -0400

    Publishing New release: 21.8.1

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,15 +6,12 @@ let package = Package(
     name: "WorkspaceOneSDK",
     platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AWSDK",
             targets: ["AWSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "AWSDK", url: "https://vmwaresaas.jfrog.io/artifactory/Workspace-ONE-iOS-SDK/MAMSDK/Release/21.6.0/AWSDK.xcframework.zip", checksum:"f7f14ab5497fbef2b1c709f3153cdd04172699d9f94e1ce22dc883b7bbda6c54")
+        .binaryTarget(name: "AWSDK", url: "https://vmwaresaas.jfrog.io/artifactory/Workspace-ONE-iOS-SDK/MAMSDK/Release/21.8.1/AWSDK.xcframework.zip", checksum:"45c3606779e7d04b27c7b909941ae173bd94f0d74b533a2f604b3adbc5ff710b")
     ]
 )
 
