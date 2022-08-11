d0d7c35d6cd0c43c51684f9ca0fa6febef821066
Vmware SDK Bot Swift Package Publisher <vmwaresdkbot@gmail.com>
2022-08-11 10:28:05 -0400

    Publishing New release: 22.7.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 //  Package.swift
 //  WorkspaceOneSDK
 //
-//  Copyright © 2021 VMware, Inc. All rights reserved. This product is protected
+//  Copyright © 2022 VMware, Inc. All rights reserved. This product is protected
 //  by copyright and intellectual property laws in the United States and other
 //  countries as well as by international treaties. VMware products are covered
 //  by one or more patents listed at http://www.vmware.com/go/patents.
@@ -21,7 +21,7 @@ let package = Package(
             targets: ["AWSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "AWSDK", url: "https://vmwaresaas.jfrog.io/artifactory/Workspace-ONE-iOS-SDK/MAMSDK/Release/22.6.0/AWSDK.xcframework.zip", checksum:"f88f08f6649a76a5a8773a8fb305916ab553d1dd7672802cf6f320e07a41e5a2")
+        .binaryTarget(name: "AWSDK", url: "https://vmwaresaas.jfrog.io/artifactory/Workspace-ONE-iOS-SDK/MAMSDK/Release/22.7.0/AWSDK.xcframework.zip", checksum:"7baa99680a912444ba3e9c0c21f05e0c545befee129b05ba2f4164d04f99dbd4")
     ]
 )
 
